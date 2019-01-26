81fa768a491de7673a84779c3966e6420b3e5127
Lawrence-Tray <lpt30@cam.ac.uk>
2019-01-26 13:15:50 +0000

    Create serial connection class

---
+++ python/test_ping_pong_serial.py
@@ -0,0 +1,20 @@
+import serial #needed for serial
+import time
+from utils.arduino_connection import Arduino_Connection
+
+
+def run():
+    arduino = Arduino_Connection(com='com9', baud_rate='9600')
+
+    while 1:
+        #send
+        message = input("input: ")
+        arduino.send_line(message)
+
+        #receive
+        received = arduino.receive_line()
+        print(received)
+
+
+if __name__ == "__main__":
+    run()
