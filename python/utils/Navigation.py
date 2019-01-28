68dede35a400499f8d1b063cb3b6221ad5a78f0c
Fernando <fernando.georgiou@gmail.com>
2019-01-28 10:58:23 +0000

    Stuff

--- python/utils/Navigation.py
+++ python/utils/Navigation.py
@@ -1,19 +1,16 @@
-class navigate:
+class Navigate:
     """Code for choosing the next block to travel to and return relative angle and distance"""
 
     def __init__(self):
         raise NotImplementedError
 
-
     def calculate_distances_angles(self, blocks, position, angle):
         """Return array of distances and relative angles"""
         raise NotImplementedError
 
-
     def choose_next_block(self, block_data, reject_blocks):
         """Return the position and relative angle of the next block to navigate to"""
         raise NotImplementedError
-
 
     def add_block_to_rejects(self, blocks, position, angle):
         """Detect rejected block when dropped behind robot and record coordinates"""
